b0d4225add085254101f5e1a002832f8336cd761
Parth Sahu <parthsahu95@gmail.com>
2022-06-13 01:50:12 +0530

    Table and Map Frontend

--- src/components/CardArea.jsx/CardArea.jsx
+++ src/components/CardArea.jsx/CardArea.jsx
@@ -1,21 +1,23 @@
-import { Divider, Stack, Box } from "@mui/material";
+import { Divider, Stack, Box, Container } from "@mui/material";
 import React from "react";
 import DataCard from "./DataCard";
 
 const CardArea = () => {
   return (
-    <Box sx={{ px: 3 }}>
-      <Stack
-        justifyContent="center"
-        direction={{ xs: "column", sm: "column", md: "row" }}
-        spacing={{ xs: 1, sm: 2, md: 4 }}
-        divider={<Divider orientation="vertical" flexItem />}
-      >
-        <DataCard heading="Active" hdColor="primary" />
-        <DataCard heading="Recovered" hdColor="green" />
-        <DataCard heading="Deceased" hdColor="red" />
-      </Stack>
-    </Box>
+    <Container>
+      <Box sx={{ px: 3, mb: 3 }}>
+        <Stack
+          justifyContent="center"
+          direction={{ xs: "column", sm: "column", md: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+          <DataCard heading="Active" hdColor="primary" />
+          <DataCard heading="Recovered" hdColor="green" />
+          <DataCard heading="Deceased" hdColor="red" />
+        </Stack>
+      </Box>
+    </Container>
   );
 };
 
