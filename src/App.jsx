c541f744870a7488ee8e9c84a3f3df8217cb473c
Parth Sahu <parthsahu95@gmail.com>
2022-06-13 01:51:25 +0530

    Table and Map further frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
 import Header from "./components/Header";
 import CardArea from "./components/CardArea.jsx/CardArea";
+import DataTable from "./components/DataTable";
+import { Box, Card, Stack } from "@mui/material";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div class="App">
       <Header />
       <CardArea />
+      <Stack direction="row">
+        <DataTable />
+
+        <Box sx={{ width: 300 }}>
+          <Card>
+            <Map />
+          </Card>
+        </Box>
+      </Stack>
     </div>
   );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import DatamapsIndia from "react-datamaps-india";
+
+const Map = () => {
+  return (
+    <DatamapsIndia
+      regionData={{
+        Maharashtra: {
+          value: 10,
+        },
+      }}
+      hoverComponent={({ value }) => {
+        return (
+          <>
+            <p>{value.name}</p>
+            <p>{value.value}</p>
+          </>
+        );
+      }}
+      mapLayout={{
+        title: "Title",
+        legendTitle: "Legend Title",
+        startColor: "#FFDAB9",
+        endColor: "#FF6347",
+        hoverTitle: "Count",
+        noDataColor: "#f5f5f5",
+        borderColor: "#8D8D8D",
+        hoverBorderColor: "#8D8D8D",
+        hoverColor: "green",
+      }}
+    />
+  );
+};
+
+export default Map;
